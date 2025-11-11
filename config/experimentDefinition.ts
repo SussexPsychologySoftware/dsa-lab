d6e7d18d4a73391414fc3ee67da0348e2cc55923
Max Lovell <max_lovell@hotmail.co.uk>
2025-11-11 17:17:26 +0000

    Swap to DSA experiment

--- config/experimentDefinition.ts
+++ config/experimentDefinition.ts
@@ -1,4 +1,5 @@
 import {ExperimentDefinition} from "@/types/experimentConfig";
 import {soundSleepDefinition} from "@/config/Example Studies/Sound Sleep/soundSleep";
+import {colourAdjuster} from "@/config/Example Studies/Colour Adjust/colourAdjust";
 
-export const experimentDefinition: ExperimentDefinition = soundSleepDefinition
+export const experimentDefinition: ExperimentDefinition = colourAdjuster
